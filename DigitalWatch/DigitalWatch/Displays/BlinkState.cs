6596c1d0fafd693cfdde95bff63462757ca042dd
Markr10 <mark-r@live.nl>
2015-04-15 21:55:03 +0200

    Enum BlinkSate added

---
+++ DigitalWatch/DigitalWatch/Displays/BlinkState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalWatch.Displays
+{
+	/// <summary>
+	/// Possible blink states of a display.
+	/// </summary>
+	public enum BlinkState
+	{
+		None,
+		Seconds,
+		Minutes,
+		Hours,
+		All
+	}
+}
+
