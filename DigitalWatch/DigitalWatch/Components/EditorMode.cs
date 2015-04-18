7a69014eebc376603881c60b20db0d408afac181
Markr10 <mark-r@live.nl>
2015-04-18 22:20:02 +0200

    EditorMode.cs added

---
+++ DigitalWatch/DigitalWatch/Components/EditorMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalWatch.Components
+{
+	/// <summary>
+	/// Possible editor modes of a component.
+	/// </summary>
+    /// <remarks>A null value should be used when the component is not in an editor mode.</remarks>
+	public enum EditorMode
+	{
+		Seconds,
+		Minutes,
+		Hours,
+	}
+}
+
