f92ee66082ba6f6e57de8ee2ed2c08f203d8b8a6
Markr10 <mark-r@live.nl>
2015-04-19 13:32:11 +0200

    DisplayChar.cs converted to DisplayTextPart.cs

--- DigitalWatch/DigitalWatch/Displays/DisplayTextPart.cs
+++ DigitalWatch/DigitalWatch/Displays/DisplayTextPart.cs
@@ -3,30 +3,30 @@
 namespace DigitalWatch.Displays
 {
     /// <summary>
-    /// A char of a display.
+    /// A part of the text for a display.
     /// </summary>
-    public struct DisplayChar
+    public struct DisplayTextPart
     {
         /// <summary>
-        /// The character that the display should show.
+        /// A part of the text that the display should show.
         /// </summary>
-        /// <value>The character that the display should show.</value>
-        public readonly char Value;
+        /// <value>A part of the text that the display should show.</value>
+        public readonly string Value;
         /// <summary>
-        /// A value indicating whether this <see cref="DigitalWatch.Displays.DisplayChar"/> should blink.
+        /// A value indicating whether this <see cref="DigitalWatch.Displays.DisplayTextPart"/> should blink.
         /// </summary>
         /// <value><c>true</c> if it should blink; otherwise, <c>false</c>.</value>
         /// <remarks>Not all displays have support for blinking.</remarks>
         public readonly bool Blink;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DigitalWatch.Displays.DisplayChar"/> struct with
+        /// Initializes a new instance of the <see cref="DigitalWatch.Displays.DisplayTextPart"/> struct with
         /// all the fields that are needed to fill the struct.
         /// </summary>
-        /// <param name="value">The character that the display should show.</param>
+        /// <param name="value">A part of the text that the display should show.</param>
         /// <param name="blink">If set to <c>true</c> it should blink.</param>
         /// <remarks>Not all displays have support for blinking.</remarks>
-        public DisplayChar(char value, bool blink)
+        public DisplayTextPart(string value, bool blink)
         {
             Value = value;
             Blink = blink;
