9cc925b42d4219906afa5f2c456fad66fdcab0ac
Markr10 <mark-r@live.nl>
2015-04-20 16:06:58 +0200

    Clock drawn

--- DigitalWatch/DigitalWatch/Displays/DialDisplayWidget.cs
+++ DigitalWatch/DigitalWatch/Displays/DialDisplayWidget.cs
@@ -13,9 +13,14 @@ namespace DigitalWatch.Displays
 		public event OnButtonPress OnSecondaryButtonPress;
 		public event OnButtonPress OnPrimaryLongButtonPress;
 
+        private double minutesAngle;
+        private double hoursAngle;
+        private double secondsAngle;
+
 		public DialDisplayWidget ()
 		{
 			this.Build ();
+            minutesAngle = hoursAngle = secondsAngle = 0.0;
 		}
 
 		protected void OnPrimaryButtonClicked (object sender, EventArgs e)
@@ -73,6 +78,7 @@ namespace DigitalWatch.Displays
 			// Set the size of the context
 			int width = area.Allocation.Width;
 			int height = area.Allocation.Height;
+            int radius = (width < height ? width : height) / 2 - 10;
 			// Set the size of the line
 			const int lineWidth = 6;
 			cr.LineWidth = lineWidth;
@@ -83,13 +89,73 @@ namespace DigitalWatch.Displays
 
 			// Draw background clock
 			cr.Translate (width / 2, height / 2);
-			cr.Arc (0, 0, (width < height ? width : height) / 2 - 10, 0, 2 * Math.PI);
+			cr.Arc (0, 0, radius, 0, 2 * Math.PI);
 			cr.SetSourceRGBA (1.0, 1.0, 1.0, 0.8); // almost white
 			cr.FillPreserve ();
 
 			cr.SetSourceRGB (0, 0, 0); // black
 			cr.Stroke ();
 
+            // Draw clock ticks
+            cr.LineCap = LineCap.Round;
+            for (int i = 0; i < 12; i++)
+            {
+                cr.Save();
+
+                // Inset height for a tick // TODO Check comment
+                double inset = 5.5;
+
+                if(i % 3 != 0)
+                {
+                    // Properties for the smaller ticks
+                    inset *= 0.8;
+                    cr.LineWidth = lineWidth / 3 * 2;
+                }
+
+                // Draw tick
+                cr.MoveTo(
+                    (radius - inset) * Math.Cos(i * Math.PI / 6),
+                    (radius - inset) * Math.Sin(i * Math.PI / 6));
+                cr.LineTo (
+                    radius * Math.Cos(i * Math.PI / 6),
+                    radius * Math.Sin(i * Math.PI / 6));
+                cr.Stroke();
+
+                // Restore the stack-pen-size
+                cr.Restore();
+            }
+
+            // Draw seconds hand
+            cr.Save();
+            cr.LineWidth = lineWidth / 3;
+            cr.SetSourceRGBA(0.7, 0.7, 0.7, 0.8); // gray
+            cr.MoveTo(0, 0);
+            cr.LineTo(
+                Math.Sin(secondsAngle) * (radius * 0.9),
+                -Math.Cos(secondsAngle) * (radius * 0.9));
+            cr.Stroke();
+            // Restore the stack-pen-size
+            cr.Restore();
+
+            // Draw minutes hand
+            cr.SetSourceRGBA(0.486, 0.149, 0.471, 0.9); // purple
+            cr.MoveTo(0, 0);
+            cr.LineTo(Math.Sin(
+                minutesAngle + secondsAngle / 60) * (radius * 0.8),
+                -Math.Cos(minutesAngle + secondsAngle / 60) * (radius * 0.8));
+            cr.Stroke();
+
+            // Draw hours hand
+            cr.SetSourceRGBA (0.729, 0.373, 0.22, 0.9); // orange
+            cr.MoveTo(0, 0);
+            cr.LineTo(
+                Math.Sin(hoursAngle + minutesAngle / 12.0) * (radius * 0.5),
+                -Math.Cos(hoursAngle + minutesAngle / 12.0) * (radius * 0.5));
+            cr.Stroke();
+
+            // Draw the middle dot
+            cr.Arc(0, 0, lineWidth / 3.0, 0, 2 * Math.PI);
+            cr.Fill();
 
 			((IDisposable) cr.GetTarget()).Dispose();                                      
 			((IDisposable) cr).Dispose();
