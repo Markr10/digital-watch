0439ad702d01e07548fa6641d0417f23f117a150
Markr10 <mark-r@live.nl>
2015-04-21 22:27:45 +0200

    Comments added

--- DigitalWatch/DigitalWatch/Program.cs
+++ DigitalWatch/DigitalWatch/Program.cs
@@ -2,8 +2,14 @@
 
 namespace DigitalWatch
 {
+    /// <summary>
+    /// Main class.
+    /// </summary>
     class MainClass
     {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
         public static void Main()
         {
             Application.Init();
